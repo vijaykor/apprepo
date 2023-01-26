878ba9968dd483489519033fe23c48990677544f
vijaykor <123626265+vijaykor@users.noreply.github.com>
2023-01-26 13:17:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,19 @@ def respond():
     # Retrieve the name from the url parameter /getmsg/?name=
     name = request.args.get("name", None)
 
-    response["MESSAGE"] = f"Welcome {name} to our awesome API!"
+    # For debugging
+    print(f"Received: {name}")
+
+    response = {}
+
+    # Check if the user sent a name at all
+    if not name:
+        response["ERROR"] = "No name found. Please send a name."
+    # Check if the user entered a number
+    elif str(name).isdigit():
+        response["ERROR"] = "The name can't be numeric. Please send a string."
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
 
     # Return the response in json format
     return jsonify(response)
